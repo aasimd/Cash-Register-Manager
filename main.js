3d947200b8cd847aef9b31cffd1134bd33252a22
aasimd <dongarkar21@gmail.com>
2022-11-27 01:53:32 +0530

    styles added

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const button = document.querySelector("#check-button");
-var message = document.querySelector(".message");
+var message = document.querySelector("#error-message");
 var returnNotes = document.querySelectorAll(".returnNotes")
 const notes = [2000, 500, 200, 100, 50, 20, 10, 5, 1]
 
 function clickHandler(){
-    console.log("clicked");
     hideMEssage ()
-    if (billAmount.value > 0){
-        if (cashGiven.value >= billAmount.value){
-            const amountToBEReturned = cashGiven.value - billAmount.value;
-            console.log(amountToBEReturned)
+   if (billAmount.value > 0) {
+
+    console.log(typeof(cashGiven.value),"cashGiven.value");
+
+    const cashGivenNumber = Number(cashGiven.value);
+    const billAmountNumber = Number(billAmount.value);
+
+    console.log(billAmount.value, 'billAmount.value')
+    
+        if (cashGivenNumber >= billAmountNumber){
+            const amountToBEReturned = cashGivenNumber - billAmountNumber;
+            console.log(amountToBEReturned);
+            console.log(typeof(cashGiven.value),"cashGiven.value")
             returnChange(amountToBEReturned);
         } else {
             errorMessage("‼ cash given is less than the bill amount")
@@ -40,10 +48,20 @@ function errorMessage (msg){
 }
 
 
-function changeHandler(){
-    var input = billAmount.value;
-    console.log(input);
-}
 
-button.addEventListener("click", clickHandler)
-billAmount.addEventListener("change", changeHandler)
+button.addEventListener("click", function clickHandler(){
+    console.log("clicked");
+    hideMEssage ()
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value){
+            const amountToBEReturned = cashGiven.value - billAmount.value;
+            console.log(amountToBEReturned);
+            returnChange(amountToBEReturned);
+        } else {
+            errorMessage("!! cash given is less than the bill amount !!")
+        }
+    } else {
+        errorMessage ("❌ invalid bill input");
+    }
+    
+});
